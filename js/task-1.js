674d56ce286b80d28d1411049eac240db7affc13
galiovets <m.galiovets@gmail.com>
2021-10-31 20:39:38 +0200

    initial

--- js/task-1.js
+++ js/task-1.js
@@ -4,8 +4,11 @@
 // миллисекунд, которое передали во время вызова функции delay.
 
 const delay = ms => {
-  // Твой код
-    console.log(ms);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  })
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
